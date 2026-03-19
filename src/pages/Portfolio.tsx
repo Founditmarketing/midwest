688bf1ab1b0c13dd3f09efad7e4bad7c2fd4fee1
Jason Albright <jason@founditmarketing.com>
2026-03-18 20:35:05 -0500

    Add 3 new gallery images (IMG_0926, IMG_1350, IMG_9750)

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -9,7 +9,7 @@ export default function Portfolio() {
   }, []);
 
   // Map through all 17 approved images, adding the two newest JPEG uploads
-  const validIndices = [1, 2, 4, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  const validIndices = [1, 2, 4, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 23, 24];
   const galleryImages = validIndices.map(i => `/images/gallery/gallery-${i}.jpg`);
 
   return (
